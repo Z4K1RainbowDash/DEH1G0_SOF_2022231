3dc09ad20006cbece4970f063d382715705a0581
Z4K1 <Z4K1@stud.uni-obuda.hu>
2022-11-01 17:50:57 +0100

    Add default Web MVC project

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DEH1G0_SOF_2022231.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
