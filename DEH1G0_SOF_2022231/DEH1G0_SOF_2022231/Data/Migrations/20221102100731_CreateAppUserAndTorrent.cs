333f895b31896bda12d68c10bbf2eb4fd07727ef
Z4K1 <Z4K1@stud.uni-obuda.hu>
2022-11-02 11:17:06 +0100

    Add migration

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Data/Migrations/20221102100731_CreateAppUserAndTorrent.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DEH1G0_SOF_2022231.Data.Migrations
+{
+    public partial class CreateAppUserAndTorrent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoContentType",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PhotoData",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Torrents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NcoreId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Downloads = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Seeders = table.Column<int>(type: "int", nullable: false),
+                    Leechers = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Torrents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TorrentUser",
+                columns: table => new
+                {
+                    AppUsersId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TorrentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TorrentUser", x => new { x.AppUsersId, x.TorrentsId });
+                    table.ForeignKey(
+                        name: "FK_TorrentUser_AspNetUsers_AppUsersId",
+                        column: x => x.AppUsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TorrentUser_Torrents_TorrentsId",
+                        column: x => x.TorrentsId,
+                        principalTable: "Torrents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TorrentUser_TorrentsId",
+                table: "TorrentUser",
+                column: "TorrentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TorrentUser");
+
+            migrationBuilder.DropTable(
+                name: "Torrents");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoContentType",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoData",
+                table: "AspNetUsers");
+        }
+    }
+}
