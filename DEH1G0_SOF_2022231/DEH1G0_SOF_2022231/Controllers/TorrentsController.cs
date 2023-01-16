3e20add3b567141ea9bb78e264f980ef3ab5b784
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-17 00:31:27 +0100

    Create Torrents controller

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DEH1G0_SOF_2022231.Controllers
+{
+    public class TorrentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
