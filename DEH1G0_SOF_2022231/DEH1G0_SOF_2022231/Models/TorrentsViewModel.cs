2a2f9bec2866ea5cb0d272296a6b96aa9f938754
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-17 00:38:55 +0100

    Add TorrentsViewModel for searching

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Models/TorrentsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DEH1G0_SOF_2022231.Models.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEH1G0_SOF_2022231.Models
+{
+    /// <summary>
+    /// The class represents the view model for displaying torrents
+    /// </summary>
+    public class TorrentsViewModel
+    {
+        /// <summary>
+        /// The text used for searching torrents
+        /// </summary>
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
+        public string SearchText { get; set; }
+
+        /// <summary>
+        /// A list of torrent categories
+        /// </summary>
+        public List<TorrentCategory> TorrentCategories { get; init; }
+    }
+
+}
