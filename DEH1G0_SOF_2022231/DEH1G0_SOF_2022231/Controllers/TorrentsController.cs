de0869da4a82962f0f919e081ee961abfcf33b55
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-20 00:56:13 +0100

    Update TorrentsContoller

--- DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DEH1G0_SOF_2022231.Data;
+using DEH1G0_SOF_2022231.Models;
+using DEH1G0_SOF_2022231.Models.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DEH1G0_SOF_2022231.Controllers
 {
     public class TorrentsController : Controller
     {
-        public IActionResult Index()
+
+
+        public TorrentsController()
         {
-            return View();
+          
         }
+        public async Task<IActionResult> SearchTorrent()
+        {
+            TorrentsViewModel vm = new TorrentsViewModel();
+            
+            return View(vm);
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchTorrent(TorrentsViewModel vm)
+        {
+            
+            return View(vm);
+
+        }
+
+
     }
 }
