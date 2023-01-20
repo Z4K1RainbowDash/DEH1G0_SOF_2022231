9009802b2094f40847f7260ec19c9739eadd2463
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-20 23:36:23 +0100

    Update Controller with Grpc and Torrent logic

--- DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
@@ -1,4 +1,5 @@
 ﻿using DEH1G0_SOF_2022231.Data;
+using DEH1G0_SOF_2022231.Logic;
 using DEH1G0_SOF_2022231.Models;
 using DEH1G0_SOF_2022231.Models.Helpers;
 using Microsoft.AspNetCore.Mvc;
@@ -7,15 +8,46 @@ namespace DEH1G0_SOF_2022231.Controllers
 {
     public class TorrentsController : Controller
     {
+        private readonly ITorrentLogic _torrentLogic;
+        private readonly IGrpcLogic _grpcLogic;
 
-
-        public TorrentsController()
+        public TorrentsController(ITorrentLogic torrentLogic, IGrpcLogic grpcLogic)
         {
-          
+            _torrentLogic = torrentLogic;
+            _grpcLogic = grpcLogic;
         }
+
         public async Task<IActionResult> SearchTorrent()
         {
             TorrentsViewModel vm = new TorrentsViewModel();
+
+
+            /* Testing data
+            List<Torrent> t = new List<Torrent>
+            {
+                new Torrent {
+                    Name = "N1",
+                    ImageUrl = "asd",
+                    Size= "1 gb",
+                    Downloads= "100",
+                    Seeders = 10,
+                    Leechers = 10
+
+                },
+
+                new Torrent {
+                    Name = "N2",
+                    ImageUrl = "asd2",
+                    Size= "12 gb",
+                    Downloads= "100",
+                    Seeders = 10,
+                    Leechers = 10
+
+                }
+            };
+            
+
+            vm.Torrents = t;*/
             
             return View(vm);
             
@@ -24,7 +56,20 @@ namespace DEH1G0_SOF_2022231.Controllers
         [HttpPost]
         public async Task<IActionResult> SearchTorrent(TorrentsViewModel vm)
         {
-            
+            var url = this._torrentLogic.GetNcoreUrl(vm.SearchText, vm.Movies, vm.Series, vm.Musics, vm.Programs, vm.Games, vm.Books);
+            var torrents = await this._grpcLogic.TorrentSearch(url);
+            vm.Torrents = torrents
+                .Select(
+                x => new Torrent { 
+                    NcoreId = int.Parse(x.Id),  
+                    Name = x.Name,
+                    ImageUrl = x.Image,
+                    Size = x.Size,
+                    Downloads = x.Downloads,
+                    Seeders = int.Parse(x.Seeders),
+                    Leechers = int.Parse(x.Leechers)
+
+                }).ToList();
             return View(vm);
 
         }
