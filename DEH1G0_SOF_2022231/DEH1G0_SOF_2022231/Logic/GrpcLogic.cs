29d149a6fd7cb3fce8f4e7895fd86646a1955301
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-24 09:45:22 +0100

    Add documentation and implement DownloadTorrent

--- DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Logic/GrpcLogic.cs
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Logic/GrpcLogic.cs
@@ -5,11 +5,19 @@ using NcoreGrpcService.Protos;
 
 namespace DEH1G0_SOF_2022231.Logic
 {
+    /// <summary>
+    /// This class handles the communication with the gRPC server.
+    /// </summary>
     public class GrpcLogic : IGrpcLogic
     {
         private string _serverAddress;
         private Ncore.NcoreClient _client;
 
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="GrpcLogic"/> class.
+        /// </summary>
+        /// <param name="serverAddress">The address of the gRPC server to connect to.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the serverAddress parameter is null</exception>
         public GrpcLogic(string serverAddress)
         {
             this._serverAddress = serverAddress ?? throw new ArgumentNullException(nameof(serverAddress));
@@ -18,6 +26,12 @@ namespace DEH1G0_SOF_2022231.Logic
 
         }
 
+        /// <summary>
+        /// Searches for torrents on a specified NcoreUrl and returns a list of TorrentDataReply.
+        /// </summary>
+        /// <param name="ncoreUrl">The NcoreUrl containing the url to search for torrents.</param>
+        /// <returns>A list of TorrentDataReply containing the search results.</returns>
+        /// <exception cref="RpcException">Thrown when the search is cancelled.</exception>
         public async Task<List<TorrentDataReply>> TorrentSearch(NcoreUrl ncoreUrl)
         {
             List<TorrentDataReply> torrents = new List<TorrentDataReply>();
@@ -37,6 +51,32 @@ namespace DEH1G0_SOF_2022231.Logic
             }
 
             return torrents;
+        }
+
+        /// <summary>
+        /// Downloads a torrent file with the given id and returns it as a MemoryStream.
+        /// </summary>
+        /// <param name="id">The id of the torrent file to download.</param>
+        /// <returns>A MemoryStream containing the downloaded torrent file.</returns>
+        public async Task<MemoryStream> DownloadTorrent(string id)
+        {
+            var request = new TorrentRequest { Id = id };
+            var memoryStream = new MemoryStream();
+
+            
+            // get file from stream
+            using (var responseStream = this._client.TorrentDownload(request))
+            {
+                while (await responseStream.ResponseStream.MoveNext())
+                {
+                    var torrentFileResponse = responseStream.ResponseStream.Current;
+                    await memoryStream.WriteAsync(torrentFileResponse.DataChunk.ToByteArray());
+                }
+            }
+
+            memoryStream.Position = 0;
+
+            return memoryStream;
         }
 
     }
