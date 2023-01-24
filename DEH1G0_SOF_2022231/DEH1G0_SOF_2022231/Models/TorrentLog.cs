858d10b1920a402424b4b16099f346436b1311dc
z4k1 <Z4K1@stud.uni-obuda.hu>
2023-01-24 21:50:54 +0100

    Implement TorrentLog class

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Models/TorrentLog.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DEH1G0_SOF_2022231.Models
+{
+    public class TorrentLog
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; }
+
+        [ForeignKey("Torrent")]
+        public string TorrentId { get; set; }
+
+        [NotMapped]
+        public virtual Torrent Torrent { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        [NotMapped]
+        public virtual AppUser User { get; set; }
+
+        public TorrentLog()
+        {
+                this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
