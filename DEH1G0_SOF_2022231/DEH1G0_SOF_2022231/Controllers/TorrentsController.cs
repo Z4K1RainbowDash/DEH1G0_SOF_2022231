610beb49dea66884f913e8eaa5a479632472cb60
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-01-26 00:38:49 +0100

    Add new Actions and dependencies

--- DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/TorrentsController.cs
@@ -2,7 +2,10 @@
 using DEH1G0_SOF_2022231.Logic;
 using DEH1G0_SOF_2022231.Models;
 using DEH1G0_SOF_2022231.Models.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace DEH1G0_SOF_2022231.Controllers
 {
@@ -10,14 +13,23 @@ namespace DEH1G0_SOF_2022231.Controllers
     {
         private readonly ITorrentLogic _torrentLogic;
         private readonly IGrpcLogic _grpcLogic;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITorrentRepository _torrentRepository;
+        private readonly ITorrentLogRepository _torrentLogRepository;
+        private readonly IAppUserRepository _appUserRepository;
 
-        public TorrentsController(ITorrentLogic torrentLogic, IGrpcLogic grpcLogic)
+        public TorrentsController(ITorrentLogic torrentLogic, IGrpcLogic grpcLogic, ITorrentRepository torrentRepository, UserManager<AppUser> userManager, ITorrentLogRepository torrentLogRepository, IAppUserRepository appUserRepository)
         {
             _torrentLogic = torrentLogic;
             _grpcLogic = grpcLogic;
+            _torrentRepository = torrentRepository;
+            _userManager = userManager;
+            _torrentLogRepository = torrentLogRepository;
+            _appUserRepository = appUserRepository;
         }
 
-        public async Task<IActionResult> SearchTorrent()
+        [Authorize]
+        public IActionResult SearchTorrent()
         {
             TorrentsViewModel vm = new TorrentsViewModel();
 
@@ -54,37 +66,75 @@ namespace DEH1G0_SOF_2022231.Controllers
         }
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> SearchTorrent(TorrentsViewModel vm)
         {
             var url = this._torrentLogic.GetNcoreUrl(vm.SearchText, vm.Movies, vm.Series, vm.Musics, vm.Programs, vm.Games, vm.Books);
             var torrents = await this._grpcLogic.TorrentSearch(url);
-            vm.Torrents = torrents
-                .Select(
-                x => new Torrent { 
-                    NcoreId = x.Id,  
-                    Name = x.Name,
-                    ImageUrl = x.Image,
-                    CreatedDateTime = x.Date,
-                    Size = x.Size,
-                    Downloads = x.Downloads,
-                    Seeders = int.Parse(x.Seeders),
-                    Leechers = int.Parse(x.Leechers)
-
-                }).ToList();
+            vm.Torrents = torrents;
+                
             return View(vm);
 
         }
 
-        public async Task<IActionResult> DownloadTorrent(string id, string name)
+        [Authorize]
+        public async Task<IActionResult> DownloadTorrent(string torrentId, string name)
         {
-            
-            var memoryStream = await this._grpcLogic.DownloadTorrent(id);
+            string realName = name.Replace('_', ' ');
+            var userId = this._userManager.GetUserId(this.User);
+
+            await this._torrentLogic.CreateIdentities(torrentId, realName, userId);
+          
+
+
+            var memoryStream = await this._grpcLogic.DownloadTorrent(torrentId);
             string torrentName = name + ".torrent";
 
             
             return File(memoryStream, "application/octet-stream", torrentName);
 
         }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ListLogs()
+        {
+
+            return View(await this._torrentLogRepository.GetAllAsync());
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> MostActiveUsersByDownloads()
+        {
+            var users = await _appUserRepository.GetAllAsync();
+            var sortedUsers = users.OrderByDescending(u => u.Torrents.Count);
+            return View(sortedUsers);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> MostPopularTorrents()
+        {
+            var torrent = await this._torrentRepository.GetAllAsync();
+            var sortedTorrents = torrent.OrderByDescending(u => u.AppUsers.Count);
+
+            return View(sortedTorrents);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> TorrentUsersByTorrent(string torrentId)
+        {
+            var users = await this._torrentRepository.GetUsersByTorrentId(torrentId);
+
+            var torrent = await this._torrentRepository.GetByIdAsync(torrentId);
+
+            TorrentUsersViewModel vm = new TorrentUsersViewModel()
+            {
+                TorrentName = torrent.Name,
+                AppUsers = users.ToList()
+
+            };
+            return View(vm);
+        }
+
 
 
     }
