04348610d0e5288f0f254b74bc957dabe0c1b5c2
Z4K1 <Z4K1@stud.uni-obuda.hu>
2023-02-13 17:52:03 +0100

    Add basic integration test for HomeController

---
+++ DEH1G0_SOF_2022231/Tests/WebsiteTests/IntegrationTests/HomeControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.WebsiteTests.TestHelpers;
+
+namespace Tests.WebsiteTests.IntegrationTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private TestingWebAppFactory<Program> _factory;
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _factory = new TestingWebAppFactory<Program>();
+            _client = _factory.CreateClient();
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Theory]
+        [TestCase("/")]
+        [TestCase("Home/Privacy")]
+        public async Task GetEndpoints_WhenCalledWithValidUrl_ShouldReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("text/html; charset=utf-8");
+        }
+    }
+}
