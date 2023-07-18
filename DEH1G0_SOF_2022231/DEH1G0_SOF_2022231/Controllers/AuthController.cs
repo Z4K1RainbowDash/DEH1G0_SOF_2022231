e3dcb70799737663f79f649f1ca963f50d96a3cc
z4k1 <Z4K1@stud.uni-obuda.hu>
2023-07-18 03:29:41 +0200

    Added login and registration endpoints to the WebAPI.

---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Controllers/AuthController.cs
@@ -0,0 +1,108 @@
+﻿using DEH1G0_SOF_2022231.Models;
+using DEH1G0_SOF_2022231.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DEH1G0_SOF_2022231.Controllers
+{
+
+    /// <summary>
+    /// This controller handles the authorisation functions.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthController"/> class.
+        /// </summary>
+        /// <param name="userManager">The user manager used to manage user data.</param>
+        /// <exception cref="ArgumentNullException"> Thrown if the provided UserManager instance is null.</exception>
+        public AuthController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        /// <summary>
+        /// Handles the user registration process.
+        /// </summary>
+        /// <param name="registerModel">The <see cref="RegisterModel"/> containing user registration information.</param>
+        /// <returns>
+        /// It returns an HTTP 200 OK response if the registration data is valid and the registration is successful, otherwise it returns an HTTP 400 Bad Request response with the validation errors.
+        /// </returns>
+        [HttpPut]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await registerModel.Image.CopyToAsync(memoryStream);
+
+
+
+                var user = new AppUser
+                {
+                    Email = registerModel.Email,
+                    UserName = registerModel.Username,
+                    LastName = registerModel.LastName,
+                    FirstName = registerModel.FirstName,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    PhotoContentType = registerModel.Image.ContentType,
+                    PhotoData = memoryStream.ToArray()
+                };
+                await _userManager.CreateAsync(user, registerModel.Password);
+                // await _userManager.AddToRoleAsync(user, "Role");
+                return Ok();
+            }
+
+
+        }
+
+        /// <summary>
+        /// Handles the user login process.
+        /// </summary>
+        /// <param name="loginModel">The <see cref="LoginModel"/> containing user login credentials.</param>
+        /// <returns>
+        /// It returns an HTTP 200 OK response with a JWT token If the login credentials are valid, otherwise it return an HTTP 401 Unauthorized response.
+        /// </returns>
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByNameAsync(loginModel.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+            {
+                var claim = new List<Claim>
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("verylongsecretkey"));
+                var token = new JwtSecurityToken(
+                 issuer: "http://www.security.org", audience: "http://www.security.org",
+                 claims: claim, expires: DateTime.Now.AddMinutes(60),
+                 signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ DEH1G0_SOF_2022231/DEH1G0_SOF_2022231/Models/Auth/RegisterModel.cs
@@ -0,0 +1,57 @@
+﻿using DEH1G0_SOF_2022231.Models.Helpers.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DEH1G0_SOF_2022231.Models.Auth
+{
+
+    /// <summary>
+    /// Represents a model for user registration.
+    /// </summary>
+    [ModelBinder(BinderType = typeof(RegisterModelBinder))]
+    public class RegisterModel
+    {
+        /// <summary>
+        /// Gets or sets the user's first name.
+        /// </summary>
+        [StringLength(200, MinimumLength = 2)]
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's last name.
+        /// </summary>
+        [StringLength(200, MinimumLength = 2)]
+        [Required]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's email address.
+        /// </summary>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's username.
+        /// </summary>
+        [StringLength(200, MinimumLength = 3)]
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's password.
+        /// </summary>
+        [StringLength(200, MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's profile image.
+        /// </summary>
+        public IFormFile Image { get; set; }
+    }
+
+}
