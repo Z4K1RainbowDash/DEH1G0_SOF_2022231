38e96f2cad14d659bdcf78eca33af6e31064aba4
z4k1 <Z4K1@stud.uni-obuda.hu>
2023-07-20 00:05:12 +0200

    Login, Register and Token model added

---
+++ DEH1G0_SOF_2022231/Angular-font-end/src/app/_models/registermodel.ts
@@ -0,0 +1,7 @@
+export class RegisterModel{
+  public firstName: string = ''
+  public lastName: string = ''
+  public email: string = ''
+  public userName: string = ''
+  public password: string = ''
+}
