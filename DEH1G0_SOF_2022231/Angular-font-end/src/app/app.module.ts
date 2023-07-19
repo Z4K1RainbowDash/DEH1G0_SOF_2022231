5c43c3a5622e995145d4ef46e0bcdf67246a5182
z4k1 <Z4K1@stud.uni-obuda.hu>
2023-07-19 17:07:38 +0200

    Componens init

--- DEH1G0_SOF_2022231/Angular-font-end/src/app/app.module.ts
+++ DEH1G0_SOF_2022231/Angular-font-end/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { TorrentsComponent } from './torrents/torrents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    FooterComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    ListUsersComponent,
+    TorrentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterOutlet,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
