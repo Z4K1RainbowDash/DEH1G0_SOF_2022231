fe9d545705ba40ea7f4824db225a8ce31c398df6
z4k1 <Z4K1@stud.uni-obuda.hu>
2023-07-27 13:55:04 +0200

    Removed unused files

--- DEH1G0_SOF_2022231/Angular-font-end/src/app/_models/my-forms/login-form.ts
+++
@@ -1,8 +0,0 @@
-import {FormControl, Validators} from "@angular/forms";
-
-export class LoginForm
-{
-    public usernameFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(3)])
-    public passwordFormControl: FormControl = new FormControl('', [Validators.required, Validators.minLength(8)])
-
-}
