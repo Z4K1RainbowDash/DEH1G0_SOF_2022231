1d3fc310ff7ff8062c9967cc39eb2c879e9463bc
Z4K1RainbowDash <Z4K1@stud.uni-obuda.hu>
2023-08-30 23:14:17 +0200

    Jwt dateCheck fixed

--- DEH1G0_SOF_2022231/Angular-font-end/src/app/api.service.ts
+++ DEH1G0_SOF_2022231/Angular-font-end/src/app/api.service.ts
@@ -1,16 +1,19 @@
 import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
+import {JwtHelperService} from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  jwtHelper: JwtHelperService
   router: Router
   readonly baseUrl: string = 'https://localhost:7235/api';
 
-  constructor(router: Router) {
+  constructor(router: Router, jwtHelper: JwtHelperService) {
     this.router = router
+    this.jwtHelper = jwtHelper
   }
 
   public isLoggedIn(): boolean {
@@ -33,60 +36,21 @@ export class ApiService {
   }
 
   private dateCheck(): boolean {
-
-    return true;
-    /*
-    const expirationDate = localStorage.getItem('nikprog-practiceapi-token-expiration');
-
-    if(expirationDate !== null)
-    {
-    // Convert JWT expiration to a Date object
-    const expirationTime = this.convertJwtExpirationToDate(expirationDate);
-
-    if (!expirationTime) {
-      // Invalid or missing expiration time, consider it as expired
-      this.removeItemsFromLocalStore()
-      return false;
+    let result:boolean = true;
+    let token = localStorage.getItem('ncore-token');
+    if (this.jwtHelper.isTokenExpired(token)) {
+      result = false;
     }
 
-    const currentTime = new Date().getTime();
-
-    // Check if the token has expired
-    if (currentTime > expirationTime.getTime()) {
-      this.removeItemsFromLocalStore()
-      return false;
-    }
-
-    return true;
-  }
-    return false;
-
-     */
+    return result;
   }
 
-  private convertJwtExpirationToDate(expiration: string): Date | null {
-    try {
-      const expirationTimeInSeconds = parseInt(expiration, 10); // Assuming the expiration is in seconds
-      const expirationTimeInMilliseconds = expirationTimeInSeconds * 1000; // Convert to milliseconds
 
-      if (isNaN(expirationTimeInMilliseconds)) {
-        throw new Error('Invalid expiration time');
-      }
-
-      return new Date(expirationTimeInMilliseconds);
-    } catch (error) {
-      console.error('Error converting JWT expiration to Date:', error);
-      return null;
-    }
-
-
-
-  }
 
   private removeItemsFromLocalStore()
   {
-    localStorage.removeItem('nikprog-practiceapi-token');
-    localStorage.removeItem('nikprog-practiceapi-token-expiration');
+    localStorage.removeItem('ncore-token');
+    localStorage.removeItem('ncore-token-expiration');
   }
 
 
